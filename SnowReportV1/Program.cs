4337b668cb917fa62a8a0c5345e238d2248203cf
Isaiah <dothackdasen@gmail.com>
2021-01-05 21:52:43 -0800

    Add project files.

---
+++ SnowReportV1/Program.cs
@@ -0,0 +1,35 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace SnowReportV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument schweitzerDoc = web.Load("https://www.schweitzer.com/discover/conditions-report/");
+            HtmlDocument mtspokaneDoc = web.Load("https://www.mtspokane.com/mountain-conditions/");
+            HtmlDocument lookoutDoc = web.Load("https://skilookout.com/snow-report");
+
+            // Schweitzer recent snow sch
+
+            HtmlNode schTime = schweitzerDoc.DocumentNode.SelectSingleNode("/html/body/main/div/div/div/div[3]/div/div/div/div/div[2]/div[2]/div[2]/div[2]/h5");
+            HtmlNode schSnow = schweitzerDoc.DocumentNode.SelectSingleNode("/html/body/main/div/div/div/div[3]/div/div/div/div/div[2]/div[2]/div[2]/div[2]/h2");
+            Console.WriteLine("Schweitzer...\n" + schTime.InnerHtml + " Snow: " + schSnow.InnerText + "\n");
+
+            // Mt Spokane recent snow spk
+
+            HtmlNode spkSnow = mtspokaneDoc.DocumentNode.SelectSingleNode("/html/body/div/section/article/div/div/div[2]/div[2]/div/text()");
+            Console.WriteLine("Mt Spokane...\n" + "24 HR Snow: " + spkSnow.InnerText.Trim() + "\"\n");
+
+            // Lookout Pass recent snow look
+
+            HtmlNode lookSnow = lookoutDoc.DocumentNode.SelectSingleNode("/html/body/section/div/div/div[2]/div/div[5]/div/div[2]/div[2]/p");
+            Console.WriteLine("Lookout Pass...\n" + "24 HR Snow: " + lookSnow.InnerHtml);
+        }
+    }
+}
